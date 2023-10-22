1d436074e9f78550fd7d718cee11d4ee5c9500f7
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-10-22 01:05:11 -0500

    edited/created the files according to the guide

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-shadow */
 console.log('Hello There!')
 
 const x =2
-const y =3 
+const y = 3
 
 console.log(x+y)
 
-const sum = (x,y) => x+y
+const sum = (x, y) => x + y
 
 console.log(sum(x,y))
